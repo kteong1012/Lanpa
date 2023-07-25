e7e5d73600a6781796c4e23cc11454a3807494df
宝鱼 <kteong1012@outlook.com>
2023-07-25 14:50:54 +0800

    [rename] 重命名局部参数

--- Assets/Lanpa/Editor/Visitors/Interfaces/IBuilderActionVisitor.cs
+++ Assets/Lanpa/Editor/Visitors/Interfaces/IBuilderActionVisitor.cs
@@ -2,33 +2,33 @@
 {
     public interface IBuilderActionVisitor<A>
     {
-        void Accept(LButtonBuilder lButtonBuilder, A a);
-        void Accept(LCheckBoxBuilder lCheckBoxBuilder, A a);
-        void Accept(LTextBuilder lTextBuilder, A a);
-        void Accept(LDropDownBuilder lDropDownBuilder, A a);
+        void Accept(LButtonBuilder builder, A a);
+        void Accept(LCheckBoxBuilder builder, A a);
+        void Accept(LTextBuilder builder, A a);
+        void Accept(LDropDownBuilder builder, A a);
     }
 
     public interface IBuilderActionVisitor<A, B>
     {
-        void Accept(LButtonBuilder lButtonBuilder, A a, B b);
-        void Accept(LCheckBoxBuilder lCheckBoxBuilder, A a, B b);
-        void Accept(LTextBuilder lTextBuilder, A a, B b);
-        void Accept(LDropDownBuilder lDropDownBuilder, A a, B b);
+        void Accept(LButtonBuilder builder, A a, B b);
+        void Accept(LCheckBoxBuilder builder, A a, B b);
+        void Accept(LTextBuilder builder, A a, B b);
+        void Accept(LDropDownBuilder builder, A a, B b);
     }
 
     public interface IBuilderActionVisitor<A, B, C>
     {
-        void Accept(LButtonBuilder lButtonBuilder, A a, B b, C c);
-        void Accept(LCheckBoxBuilder lCheckBoxBuilder, A a, B b, C c);
-        void Accept(LTextBuilder lTextBuilder, A a, B b, C c);
-        void Accept(LDropDownBuilder lDropDownBuilder, A a, B b, C c);
+        void Accept(LButtonBuilder builder, A a, B b, C c);
+        void Accept(LCheckBoxBuilder builder, A a, B b, C c);
+        void Accept(LTextBuilder builder, A a, B b, C c);
+        void Accept(LDropDownBuilder builder, A a, B b, C c);
     }
 
     public interface IBuilderActionVisitor<A, B, C, D>
     {
-        void Accept(LButtonBuilder lButtonBuilder, A a, B b, C c, D d);
-        void Accept(LCheckBoxBuilder lCheckBoxBuilder, A a, B b, C c, D d);
-        void Accept(LTextBuilder lTextBuilder, A a, B b, C c, D d);
-        void Accept(LDropDownBuilder lDropDownBuilder, A a, B b, C c, D d);
+        void Accept(LButtonBuilder builder, A a, B b, C c, D d);
+        void Accept(LCheckBoxBuilder builder, A a, B b, C c, D d);
+        void Accept(LTextBuilder builder, A a, B b, C c, D d);
+        void Accept(LDropDownBuilder builder, A a, B b, C c, D d);
     }
 }
